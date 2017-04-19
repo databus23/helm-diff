806a6e2c3d4bd93f50e6d6bdb7f1aea7a2bb1890
Fabian Ruff <fabian@progra.de>
2017-04-20 00:25:40 +0200

    Add install hook to plugin.yaml
    This hopefully fixes the helm-plugin install command.

---
+++ install-binary.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Shamelessly copied from https://github.com/technosophos/helm-template
+
+PROJECT_NAME="helm-diff"
+PROJECT_GH="databus23/$PROJECT_NAME"
+
+: ${HELM_PLUGIN_PATH:="$(helm home)/plugins/helm-diff"}
+
+if [[ $SKIP_BIN_INSTALL == "1" ]]; then
+  echo "Skipping binary install"
+  exit
+fi
+
+# initArch discovers the architecture for this system.
+initArch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    armv5*) ARCH="armv5";;
+    armv6*) ARCH="armv6";;
+    armv7*) ARCH="armv7";;
+    aarch64) ARCH="arm64";;
+    x86) ARCH="386";;
+    x86_64) ARCH="amd64";;
+    i686) ARCH="386";;
+    i386) ARCH="386";;
+  esac
+}
+
+# initOS discovers the operating system for this system.
+initOS() {
+  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+
+  case "$OS" in
+    # Minimalist GNU for Windows
+    mingw*) OS='windows';;
+    darwin) OS='macos';;
+  esac
+}
+
+# verifySupported checks that the os/arch combination is supported for
+# binary builds.
+verifySupported() {
+  local supported="linux-amd64\nmacos-amd64"
+  if ! echo "${supported}" | grep -q "${OS}-${ARCH}"; then
+    echo "No prebuild binary for ${OS}-${ARCH}."
+    exit 1
+  fi
+
+  if ! type "curl" > /dev/null && ! type "wget" > /dev/null; then
+    echo "Either curl or wget is required"
+    exit 1
+  fi
+}
+
+# getDownloadURL checks the latest available version.
+getDownloadURL() {
+  # Use the GitHub API to find the latest version for this project.
+  local latest_url="https://api.github.com/repos/$PROJECT_GH/releases/latest"
+  if type "curl" > /dev/null; then
+    DOWNLOAD_URL=$(curl -s $latest_url | grep $OS | awk '/\"browser_download_url\":/{gsub( /[,\"]/,"", $2); print $2}')
+  elif type "wget" > /dev/null; then
+    DOWNLOAD_URL=$(wget -q -O - $latest_url | awk '/\"browser_download_url\":/{gsub( /[,\"]/,"", $2); print $2}')
+  fi
+}
+
+# downloadFile downloads the latest binary package and also the checksum
+# for that binary.
+downloadFile() {
+  PLUGIN_TMP_FILE="/tmp/${PROJECT_NAME}.tgz"
+  echo "Downloading $DOWNLOAD_URL"
+  if type "curl" > /dev/null; then
+    curl -L "$DOWNLOAD_URL" -o "$PLUGIN_TMP_FILE"
+  elif type "wget" > /dev/null; then
+    wget -q -O "$PLUGIN_TMP_FILE" "$DOWNLOAD_URL"
+  fi
+}
+
+# installFile verifies the SHA256 for the file, then unpacks and
+# installs it.
+installFile() {
+  HELM_TMP="/tmp/$PROJECT_NAME"
+  mkdir -p "$HELM_TMP"
+  tar xf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
+  HELM_TMP_BIN="$HELM_TMP/diff"
+  echo "Preparing to install into ${HELM_PLUGIN_PATH}"
+  cp "$HELM_TMP_BIN" "$HELM_PLUGIN_PATH"
+}
+
+# fail_trap is executed if an error occurs.
+fail_trap() {
+  result=$?
+  if [ "$result" != "0" ]; then
+    echo "Failed to install $PROJECT_NAME"
+    echo "\tFor support, go to https://github.com/databus23/helm-diff."
+  fi
+  exit $result
+}
+
+# testVersion tests the installed client to make sure it is working.
+testVersion() {
+  set +e
+  echo "$PROJECT_NAME installed into $HELM_PLUGIN_PATH/$PROJECT_NAME"
+  $HELM_PLUGIN_PATH/diff -h
+  set -e
+}
+
+# Execution
+
+#Stop execution on any error
+trap "fail_trap" EXIT
+set -e
+initArch
+initOS
+verifySupported
+getDownloadURL
+downloadFile
+installFile
+testVersion
