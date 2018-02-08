3fc923449315708f401a1fe02306f0586c9afe4e
aananthraj <aananthraj1995@gmail.com>
2018-02-08 05:38:07 +0530

    syncig with upstream master

--- main.go
+++ main.go
@@ -21,6 +21,11 @@ The Helm Diff Plugin
   and compares it to a local chart plus values. This can be 
   used visualize what changes a helm upgrade will perform.
 
+* Shows a diff explaing what a helm rollback would change:
+    This fetches the currently deployed version of a release
+  and compares it rollback revision. This can be 
+  used visualize what changes a helm rollback will perform.
+
 * Shows a diff explaing what had changed between two revisions:
     This fetches previously deployed versions of a release
   and compares them. This can be used visualize what changes 
@@ -51,7 +56,7 @@ with values generated form charts.
 It forecasts/visualizes changes, that a helm upgrade could perform.
 `
 	upgradeCmd := &cobra.Command{
-		Use:   "upgrade [flags] [RELEASE] [CHART]",
+		Use:   "upgrade [RELEASE] [CHART] [flags]",
 		Short: "visualize changes, that a helm upgrade could perform",
 		Long:  upgradeCmdLongUsage,
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -87,16 +92,53 @@ It forecasts/visualizes changes, that a helm upgrade could perform.
 	f.BoolVar(&diff.reuseValues, "reuse-values", false, "reuse the last release's values and merge in any new values")
 	f.StringArrayVar(&diff.suppressedKinds, "suppress", []string{}, "allows suppression of the values listed in the diff output")
 
+	const rollbackCmdLongUsage = `
+This command compares the laset manifests details of a named release 
+with specific revision values to rollback.
+
+It forecasts/visualizes changes, that a helm rollback could perform.
+`
+	rollbackCmd := &cobra.Command{
+		Use:   "rollback [flags] [RELEASE] [REVISION]",
+		Short: "visualize changes, that a helm rollback could perform",
+		Long:  rollbackCmdLongUsage,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if v, _ := cmd.Flags().GetBool("version"); v {
+				fmt.Println(Version)
+				return nil
+			}
+
+			if err := checkArgsLength(len(args), "release name", "revision number"); err != nil {
+				return err
+			}
+
+			if q, _ := cmd.Flags().GetBool("suppress-secrets"); q {
+				diff.suppressedKinds = append(diff.suppressedKinds, "Secret")
+			}
+
+			diff.release = args[0]
+			diff.revisions = args[1:]
+
+			if diff.client == nil {
+				diff.client = helm.NewClient(helm.Host(os.Getenv("TILLER_HOST")))
+			}
+
+			return diff.backcast()
+		},
+	}
+
+	rollbackCmd.SuggestionsMinimumDistance = 1
+
 	const revisionCmdLongUsage = `
 This command compares the manifests details of a named release.
 
 It can be used to compare the manifests of 
  
  - lastest REVISION with specified REVISION
-	$ helm diff revision [flags] [RELEASE] REVISION
+	$ helm diff revision [flags] [RELEASE] [REVISION]
 
  - REVISION1 with REVISION2
-	$ helm diff revision [flags] [RELEASE] REVISION1 REVISION1
+	$ helm diff revision [flags] [RELEASE] [REVISION1] [REVISION2]
 `
 
 	revisionCmd := &cobra.Command{
@@ -145,14 +187,16 @@ It can be used to compare the manifests of
 	}
 
 	rootCmd := &cobra.Command{
-		Use:   "diff",
-		Short: "Show differences between release revisions",
-		Long:  globalUsage,
+		Use:          "diff",
+		Short:        "Show differences between release revisions",
+		Long:         globalUsage,
+		SilenceUsage: true,
 	}
 
 	rootCmd.AddCommand(
 		upgradeCmd,
 		revisionCmd,
+		rollbackCmd,
 		versionCmd,
 	)
 
@@ -161,16 +205,7 @@ It can be used to compare the manifests of
 	}
 }
 
-func (d *diffCmd) forecast() error {
-	chartPath, err := locateChartPath(d.chart, "", false, "")
-	if err != nil {
-		return err
-	}
-
-	rawVals, err := d.vals()
-	if err != nil {
-		return err
-	}
+func (d *diffCmd) backcast() error {
 
 	releaseResponse, err := d.client.ReleaseContent(d.release)
 
@@ -178,21 +213,13 @@ func (d *diffCmd) forecast() error {
 		return prettyError(err)
 	}
 
-	upgradeResponse, err := d.client.UpdateRelease(
-		d.release,
-		chartPath,
-		helm.UpdateValueOverrides(rawVals),
-		helm.ReuseValues(d.reuseValues),
-		helm.UpgradeDryRun(true),
-	)
+	revision, _ := strconv.Atoi(d.revisions[0])
+	revisionResponse, err := d.client.ReleaseContent(d.release, helm.ContentReleaseVersion(int32(revision)))
 	if err != nil {
 		return prettyError(err)
 	}
 
-	currentSpecs := manifest.Parse(releaseResponse.Release.Manifest)
-	newSpecs := manifest.Parse(upgradeResponse.Release.Manifest)
-
-	diffManifests(currentSpecs, newSpecs, d.suppressedKinds, os.Stdout)
+	diffManifests(manifest.Parse(revisionResponse.Release.Manifest), manifest.Parse(releaseResponse.Release.Manifest), d.suppressedKinds, os.Stdout)
 
 	return nil
 }
@@ -237,6 +264,42 @@ func (d *diffCmd) differentiate() error {
 	default:
 		return errors.New("Invalid Arguments")
 	}
+
+	return nil
+}
+
+func (d *diffCmd) forecast() error {
+	chartPath, err := locateChartPath(d.chart, "", false, "")
+	if err != nil {
+		return err
+	}
+
+	rawVals, err := d.vals()
+	if err != nil {
+		return err
+	}
+
+	releaseResponse, err := d.client.ReleaseContent(d.release)
+
+	if err != nil {
+		return prettyError(err)
+	}
+
+	upgradeResponse, err := d.client.UpdateRelease(
+		d.release,
+		chartPath,
+		helm.UpdateValueOverrides(rawVals),
+		helm.ReuseValues(d.reuseValues),
+		helm.UpgradeDryRun(true),
+	)
+	if err != nil {
+		return prettyError(err)
+	}
+
+	currentSpecs := manifest.Parse(releaseResponse.Release.Manifest)
+	newSpecs := manifest.Parse(upgradeResponse.Release.Manifest)
+
+	diffManifests(currentSpecs, newSpecs, d.suppressedKinds, os.Stdout)
 
 	return nil
 }
