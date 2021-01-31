57dc095ab12e3599fe576b1993950c077b8c63cd
Josh Soref <jsoref@users.noreply.github.com>
2021-01-31 05:36:36 -0500

    spelling: loading
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- diff/report.go
+++ diff/report.go
@@ -150,7 +150,7 @@ func newTemplate(name string) *template.Template {
 func setupJSONReport(r *Report) {
 	t, err := newTemplate("entries").Parse(defaultTemplateReport)
 	if err != nil {
-		log.Fatalf("Error loadding default template: %v", err)
+		log.Fatalf("Error loading default template: %v", err)
 	}
 
 	r.format.output = templateReportPrinter(t)
@@ -170,14 +170,14 @@ func setupTemplateReport(r *Report) {
 			t, err := newTemplate(filepath.Base(tplFile)).ParseFiles(tplFile)
 			if err != nil {
 				fmt.Println(err)
-				log.Fatalf("Error loadding custom template")
+				log.Fatalf("Error loading custom template")
 			}
 			tpl = t
 		} else {
 			// Render
 			t, err := newTemplate("entries").Parse(defaultTemplateReport)
 			if err != nil {
-				log.Fatalf("Error loadding default template")
+				log.Fatalf("Error loading default template")
 			}
 			tpl = t
 		}
