19a52076ddc2267b97c0c5580a2462a521973857
Jan-Otto Kröpke <github@jkroepke.de>
2022-01-09 05:03:21 +0100

    Use cygpath for HELM_BIN (#287)
    Since https://github.com/databus23/helm-diff/commit/7f10f255cdea7270b7916e103b532fbb1687ea69 HELM_BIN instead helm is used.
    
    If helm plugin install is running on cygwin, an error occurred:
    
    ```
    # Installed plugin: secrets
    ...
    # D:\a\helm-secrets\helm-secrets\tests\.tmp\cache\CYGWIN_NT-10.0\helm\home\helm\plugins\helm-diff/install-binary.sh: line 9: C:\hostedtoolcache\windows\helm\3.6.0\x64\windows-amd64\helm.exe: command not found
    ...
    
    # Error: plugin install hook for "diff" exited with error
    ```
    
    Like HELM_PLUGIN_DIR, HELM_BIN needs to be translated on cygwin, too.

--- install-binary.sh
+++ install-binary.sh
@@ -6,6 +6,16 @@ PROJECT_NAME="helm-diff"
 PROJECT_GH="databus23/$PROJECT_NAME"
 export GREP_COLOR="never"
 
+# Convert HELM_BIN and HELM_PLUGIN_DIR to unix if cygpath is
+# available. This is the case when using MSYS2 or Cygwin
+# on Windows where helm returns a Windows path but we
+# need a Unix path
+
+if type cygpath >/dev/null 2>&1; then
+  HELM_BIN="$(cygpath -u "${HELM_BIN}")"
+  HELM_PLUGIN_DIR="$(cygpath -u "${HELM_PLUGIN_DIR}")"
+fi
+
 [ -z "$HELM_BIN" ] && HELM_BIN=$(which helm)
 HELM_MAJOR_VERSION=$("${HELM_BIN}" version --client --short | awk -F '.' '{print $1}')
 
@@ -15,15 +25,6 @@ HELM_HOME=$("${HELM_BIN}" home --debug=false)
 mkdir -p "$HELM_HOME"
 
 : ${HELM_PLUGIN_DIR:="$HELM_HOME/plugins/helm-diff"}
-
-# Convert the HELM_PLUGIN_DIR to unix if cygpath is
-# available. This is the case when using MSYS2 or Cygwin
-# on Windows where helm returns a Windows path but we
-# need a Unix path
-
-if type cygpath >/dev/null 2>&1; then
-  HELM_PLUGIN_DIR=$(cygpath -u $HELM_PLUGIN_DIR)
-fi
 
 if [ "$SKIP_BIN_INSTALL" = "1" ]; then
   echo "Skipping binary install"
