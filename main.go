de536b3ce96ced5b6e849acb9eea0d85255b71cb
Yusuke Kuoka <ykuoka@gmail.com>
2022-01-22 16:37:28 +0900

    Add various client-go auth providers for 3-way merge (#355)
    * Add various client-go auth providers for 3-way merge
    
    Fixes #354

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"os"
 
 	"github.com/databus23/helm-diff/v3/cmd"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/exec"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
 func main() {
