b83ce22a3cf4360c823ccfd94505fc4736a2c418
Yusuke Kuoka <ykuoka@gmail.com>
2023-06-04 02:46:15 +0000

    Fix release.sh to work with sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 set -e
-if [ "$1" == "" ]; then
+
+# == would end up with: scripts/release.sh: 5: [: v3.8.1: unexpected operator
+if [ "$1" = "" ]; then
   echo usage: "$0 VERSION"
 fi
+
 git tag $1
 git push origin $1
 gh release create $1 --draft --generate-notes --title "$1" release/*.tgz
