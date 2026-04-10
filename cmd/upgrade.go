89356c432d40664967cdf1917b49b500073c6115
yxxhero <aiopsclub@163.com>
2026-04-10 22:49:27 +0800

    fix: support multi-file KUBECONFIG with three-way merge (#969)
    The three-way merge code path used a shallow copy of EnvSettings which
    caused ConfigFlags.KubeConfig to point to the wrong struct. When
    KUBECONFIG contained multiple colon-separated paths, the entire string
    was passed as ExplicitPath to the k8s client loader, causing os.Stat()
    to fail with 'no such file or directory'.
    
    Fix by creating a fresh EnvSettings and clearing KubeConfig when it
    contains multiple paths, allowing the default loading rules to handle
    multi-file KUBECONFIG via the Precedence field.
    
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"slices"
 	"strconv"
 	"strings"
@@ -297,11 +298,7 @@ func (d *diffCmd) runHelm3() error {
 	var actionConfig *action.Configuration
 	if d.threeWayMerge || d.takeOwnership {
 		actionConfig = new(action.Configuration)
-		localEnv := cli.New()
-		*localEnv = *envSettings
-		if d.kubeContext != "" {
-			localEnv.KubeContext = d.kubeContext
-		}
+		localEnv := prepareEnvSettings(d.kubeContext)
 		if err := actionConfig.Init(localEnv.RESTClientGetter(), localEnv.Namespace(), os.Getenv("HELM_DRIVER")); err != nil {
 			log.Fatalf("%+v", err)
 		}
@@ -408,4 +405,15 @@ func checkOwnership(d *diffCmd, resources kube.ResourceList, currentSpecs map[st
 		return nil
 	})
 	return newOwnedReleases, err
+}
+
+func prepareEnvSettings(kubeContext string) *cli.EnvSettings {
+	localEnv := cli.New()
+	if len(filepath.SplitList(localEnv.KubeConfig)) > 1 {
+		localEnv.KubeConfig = ""
+	}
+	if kubeContext != "" {
+		localEnv.KubeContext = kubeContext
+	}
+	return localEnv
 }
